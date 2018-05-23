91d4d0e00ab310933dc79990d93f241de40ae26d
Wolfdrakenn <pierre.huvelin@epsi.fr>
2018-05-23 16:27:47 +0200

    transfo form par contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Contact;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Surname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Comment;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->Contact;
+    }
+
+    public function setContact(string $Contact): self
+    {
+        $this->Contact = $Contact;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->Surname;
+    }
+
+    public function setSurname(string $Surname): self
+    {
+        $this->Surname = $Surname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->Comment;
+    }
+
+    public function setComment(string $Comment): self
+    {
+        $this->Comment = $Comment;
+
+        return $this;
+    }
+}
