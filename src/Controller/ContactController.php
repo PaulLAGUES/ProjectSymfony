5012cb5c2166351bcd7b7d3177d7a97bdc0ebdd9
Wolfdrakenn <pierre.huvelin@epsi.fr>
2018-05-24 11:35:30 +0200

    Creation dossier Form et formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactRepository;
 
+use App\Form\ReviewType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,11 +13,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ContactController extends Controller
 {
     /**
-     * @Route("/contact/{id}/create", name="contact_create")
+     * @Route("/contact", name="contact_create")
      */
     public function createContact($id, Request $request)
     {
-        return $this->render('contact/contact.html.twig', [
+        //crée une instance de de Contact vide
+        $contact=new Contact();
+
+        //créé le formulaire et lui associe notre instance vide
+        $form = $this->createForm(ReviewType::class, $contact);
+
+        //prend les données du formulaire et les injecte dans le review vide
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //sauvegarde du commentaire
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            //ce message est censée s'afficher sur la page en validation du commentaire
+            $this->addFlash("success", "Ton commentaire est enregistré, une réponse te sera envoyée rapidement!");
+        }
+
+        return $this->render("contact/contact.html.twig", [
+            "form" =>$form->createView(),
             'controller_name' => 'ContactController',
         ]);
     }
